7ef3bfb91b30f0964694fde680801c48fef0b7ce
Sudesh Nimesha <sudeshnt93@gmail.com>
2025-04-19 23:00:17 +0700

    feat: add logic to create PR with the custom JS action

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,5 +1,6 @@
 const core = require("@actions/core"); // provide utilities to execute command line scripts
 const exec = require("@actions/exec"); // provide a lot of functionality to interact with github api & get access to a lot of information from contexts within our action
+const github = require("@actions/github"); // provide a lot of functionality to interact with github api & get access to a lot of information from contexts within our action
 
 const validateBranchName = (branchName) =>
   /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
@@ -21,12 +22,26 @@ async function run() {
       4.2 Create a PR to the base-branch using the octokit API
     5. Otherwise, conclude the custom action
   */
-  const baseBranch = core.getInput("base-branch");
-  const targetBranch = core.getInput("target-branch");
-  const ghToken = core.getInput("gh-token");
-  const workingDirectory = core.getInput("working-directory");
+  const baseBranch = core.getInput("base-branch", {
+    required: true,
+  });
+  const targetBranch = core.getInput("target-branch", {
+    required: true,
+  });
+  const ghToken = core.getInput("gh-token", {
+    required: true,
+  });
+  const workingDirectory = core.getInput("working-directory", {
+    required: true,
+  });
   const debug = core.getBooleanInput("debug");
 
+  const commonExecOptions = {
+    cwd: workingDirectory,
+    env: {
+      GITHUB_TOKEN: ghToken,
+    },
+  };
   core.setSecret(ghToken);
 
   if (!validateBranchName(baseBranch)) {
@@ -56,20 +71,47 @@ async function run() {
     `[js-dependency-update] - Working directory is ${workingDirectory}`
   );
 
-  await exec.exec("npm update", [], {
-    cwd: workingDirectory,
-  });
+  await exec.exec("npm update", { ...commonExecOptions });
 
-  const gitStatus = await exec.getExecOutput(
-    "git status -s package*.json",
-    [],
-    {
-      cwd: workingDirectory,
-    }
-  );
+  const gitStatus = await exec.getExecOutput("git status -s package*.json", {
+    ...commonExecOptions,
+  });
 
   if (gitStatus.stdout.length > 0) {
     core.info("[js-dependency-update] - There are updates available!");
+    await exec.exec("git config --global user.name 'sudeshnt'");
+    await exec.exec("git config --global user.email 'sudeshnt93@gmail.com'");
+    await exec.exec(`git checkout -b ${targetBranch}`, [], {
+      ...commonExecOptions,
+    });
+    await exec.exec("git add package.json package-lock.json", [], {
+      ...commonExecOptions,
+    });
+    await exec.exec("git commit -m 'chore: update dependencies'", [], {
+      ...commonExecOptions,
+    });
+    await exec.exec("git push -u origin ${targetBranch} --force", [], {
+      // for a real repo, we may not use force push, instead rebase the target branch with the base branch
+      ...commonExecOptions,
+    });
+
+    const octokit = new github.getOctokit(ghToken);
+
+    try {
+      await octokit.rest.pulls.create({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        base: baseBranch,
+        head: targetBranch,
+        title: "chore: update npm dependencies",
+        body: "This PR updates the dependencies to the latest versions.",
+      });
+    } catch (error) {
+      core.error(error);
+      core.setFailed(
+        `[js-dependency-update] Failed to create PR: ${error.message}. Please check the target branch and the base branch.`
+      );
+    }
   } else {
     core.info("[js-dependency-update] - No updates at this point in time!");
   }
